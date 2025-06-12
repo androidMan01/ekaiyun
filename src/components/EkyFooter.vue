93cf80c8228f4765cf87579784e4ec39401a283a
NEU-20230907XSE\lsh <lv-shh@neusoft.com>
2025-06-12 10:50:46 +0800

    update

--- src/components/EkyFooter.vue
+++ src/components/EkyFooter.vue
@@ -5,12 +5,12 @@
         地&nbsp;&nbsp;&nbsp;&nbsp;址：浙江省义乌市福田街道城北路699号宏基大厦7楼705-707室
       </div>
       <div>
-        联系人：吴可可销售经理 手机：13429028788 QQ:554438788 <br>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;孙孟典总经理 手机：15057822888<br>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;郭政副总经理 手机：18554858948  邮箱：zguo@f5strans.ru
+        联系人：吴可可（销售经理） 手机：13429028788 QQ:554438788 <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;孙孟典（总经理） 手机：15057822888<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;郭政副（总经理） 手机：18554858948  邮箱：zguo@f5strans.ru
       </div>
       <div>
-        <span>邮&nbsp;&nbsp;&nbsp;&nbsp;件：keith@ekaiyun.cc</span>
+        <span>邮&nbsp;&nbsp;&nbsp;&nbsp;箱：keith@ekaiyun.cc</span>
       </div>
       <div class="footer-line"></div>
       <div>Copyright © 2017-2025 浙江俄凯运供应链管理有限公司 (ZheJiang Ekaiyun Supply Chain Management Co.,Ltd.) All Rights Reserved 浙ICP备19019857号-2</div>
